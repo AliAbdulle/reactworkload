1bec6e91cbb109db7c4d317cd864b0b5c13b7320
AliAbdulle <alidhobey@gmail.com>
2019-12-05 20:08:07 -0600

    create search buttom

--- src/components/SearchWorks.js
+++ src/components/SearchWorks.js
@@ -3,7 +3,33 @@ import React, {Component} from 'react';
 class SearchWorks extends Component {
 
     render(){
-        return  <div> Search Works </div>;
+        return (
+            <div className= "search-works row justify-content-center my-4">
+                <div className="col-md-6">
+                    <div className="input-group">
+                        <input
+                            id="searchWorks"
+                            type="text"
+                            className="form-control"
+                            arial-label="search Works"
+                            />
+                    <div className="input-group-append">
+                        <button
+                            type="button"
+                            className="btn btn-primary dropdown-toggle"
+                            data-toggle="dropdown"
+                            arial--baspoup="true"
+                            arial-expanded="false"
+                            >
+                                Sort by: <span className="caret"/>
+                            </button>
+                    </div>
+                    </div>
+                </div>
+
+
+            </div> 
+        )
 
     }
 }
