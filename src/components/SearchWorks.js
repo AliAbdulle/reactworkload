ccc74bf2c4815a302dd4c16aa0b430a2ff2a50fd
AliAbdulle <alidhobey@gmail.com>
2019-12-05 20:17:18 -0600

    Completed Search button

--- src/components/SearchWorks.js
+++ src/components/SearchWorks.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class SearchWorks extends Component {
 
-    render(){
+    render() {
         return (
-            <div className= "search-works row justify-content-center my-4">
+            <div className="search-works row justify-content-center my-4">
                 <div className="col-md-6">
                     <div className="input-group">
                         <input
@@ -12,23 +12,39 @@ class SearchWorks extends Component {
                             type="text"
                             className="form-control"
                             arial-label="search Works"
-                            />
-                    <div className="input-group-append">
-                        <button
-                            type="button"
-                            className="btn btn-primary dropdown-toggle"
-                            data-toggle="dropdown"
-                            arial--baspoup="true"
-                            arial-expanded="false"
+                        />
+                        <div className="input-group-append">
+                            <button
+                                type="button"
+                                className="btn btn-primary dropdown-toggle"
+                                data-toggle="dropdown"
+                                arial--baspoup="true"
+                                arial-expanded="false"
                             >
-                                Sort by: <span className="caret"/>
+                                Sort by: <span className="caret" />
                             </button>
-                    </div>
+                            <div className="sort-menu dropdown-menu dropdown-menue- right">
+                                <button className="sort-by dropdown-item" href="#">
+                                    Title
+                                </button>
+                                <button className="sort-by dropdown-item" href="#">
+                                    Full Name
+                                </button>
+                                <div role="separetor" className="dropdown-divider" />
+                                <button className="sort-by dropdown-item" href="#">
+                                    Asc
+                                </button>
+                                <button className="sort-by dropdown-item" href="#">
+                                    Desc
+                                </button>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
 
 
-            </div> 
+            </div>
         )
 
     }
